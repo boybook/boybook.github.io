14c30f54b3c979dc597dc290e0229c56b1963ac8
boybook <fangyizhou1997@qq.com>
2019-06-08 14:58:00 +0800

    优化淡出效果

--- js/scroll.js
+++ js/scroll.js
@@ -1,25 +1,17 @@
 $(document).ready(function(){
-    var tops = [];
-    $(".fadeout").each(function(i, e) {
-        tops[i] = $(this).offset().top;
-    });
-    //alert(tops);
-    $(".fadeout").hide();
 	var h3_height = document.documentElement.clientHeight;
-	
     var onScroll = function(){
 		var this_scrollTop = $(this).scrollTop();
 		
 		var scroll = this_scrollTop + (h3_height / 1.2);
         $(".fadeout").each(function(i, e) {
-            var top = tops[i];
-            if (scroll >= top && $(this).is(':hidden')) {
-                $(this).show();
+            var top = $(this).offset().top;
+            if (scroll >= top) {
+                $(this).css("animation-play-state", "running");
                 //alert("top=" + top + "\noffsettop=" + $(this).offset().top + "\nscroll=" + scroll + "\n展示啦！")
             }
         })
 	};
-    
     onScroll();
 	$(window).scroll(onScroll);
 	
